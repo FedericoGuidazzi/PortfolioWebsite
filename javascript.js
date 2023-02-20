a86966fb06361b24952c6882bc194e10df11750d
Federico Guidazzi <federico.guidazzi@gft.com>
2023-02-20 12:37:39 +0100

    finish navbar

---
+++ javascript.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#body').css("margin-top", parseInt($('.navbar').height())+parseInt($('.navbar').css('padding-top'))+parseInt($('.navbar').css('padding-bottom')));
+});
